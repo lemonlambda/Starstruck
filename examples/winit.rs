9defd973a97b5aac1fb892a495519796b27eb989
LemonjamesD <lemon@lemonjamesd.com>
2023-10-16 17:27:35 -0400

    Stuff and things

--- examples/winit.rs
+++ examples/winit.rs
@@ -7,5 +7,5 @@ fn test_system(_world: &World) {
 }
 
 fn main() {
-    World::new().add_resource(StarstruckEngine::new("Winit Test")).add_system(DefaultOrdering::Run, test_system).add_startup_system(run_winit).start().run();
+    World::new().add_resource(StarstruckEngine::new("Winit Test")).add_system(DefaultOrdering::Run, test_system).add_startup_system(create_window).start().run();
 }
