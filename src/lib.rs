e4e34a1205a11b694eb017a3e4ddfc043e8565ae
LemonjamesD <lemon@lemonjamesd.com>
2023-10-17 09:07:45 -0400

    Added all of the renderer

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+pub mod renderer;
+
+use renderer::Renderer;
+use winit::dpi::PhysicalSize;
+
 use std::thread::spawn;
 use std::sync::{Arc, Mutex};
 use std::borrow::Cow;
@@ -8,7 +13,7 @@ use wgpu::Instance;
 use winit::event_loop::{EventLoopBuilder, EventLoop, ControlFlow};
 use winit::platform::wayland::EventLoopBuilderExtWayland;
 use winit::window::WindowBuilder;
-use winit::event::{Event, WindowEvent};
+use winit::event::{Event, WindowEvent, ElementState, VirtualKeyCode, KeyboardInput};
 
 #[derive(Clone)]
 pub struct StarstruckEngine {
@@ -31,17 +36,56 @@ impl StarstruckEngine {
             world: World::new()
         }
     }
-    pub async fn run(&mut self) -> &mut Self {
-        run_winit().await;
-        self
+    pub async fn run(self) {
+        let event_loop = EventLoop::new();
+        let window = winit::window::Window::new(&event_loop).unwrap();
+
+        let mut renderer = Renderer::new(window).await;
+
+        event_loop.run(move |event, _, control_flow| {
+            match event {
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == renderer.window().id() => if !renderer.input(event) { // UPDATED!
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            let PhysicalSize { width: x, height: y } = *physical_size;
+                            renderer.resize(x, y);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            let PhysicalSize { width: x, height: y } = **new_inner_size;
+                            renderer.resize(x, y);
+                        }
+                        _ => {}
+                    }
+                },
+                Event::RedrawRequested(window_id) if window_id == renderer.window().id() => {
+                    renderer.update();
+                    match renderer.render() {
+                        Ok(_) => {}
+                        // Reconfigure the surface if lost
+                        Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size.width, renderer.size.height),
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                        // All other errors (Outdated, Timeout) should be resolved by the next frame
+                        Err(e) => eprintln!("{:?}", e),
+                    }
+                }
+                _ => {}
+            }
+        })
     }
 }
 
 impl Resource for StarstruckEngine {}
-
-async fn run_winit() {
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
-
-
-}
---
+++ src/renderer.rs
@@ -0,0 +1,138 @@
+use wgpu::{Surface, Device, Queue, SurfaceConfiguration, Instance, InstanceDescriptor, Backends, Adapter, RequestAdapterOptions, PowerPreference, TextureUsages, DeviceDescriptor, SurfaceError, TextureViewDescriptor, CommandEncoderDescriptor, Color, RenderPassColorAttachment};
+use winit::{dpi::PhysicalSize, window::Window, event::WindowEvent};
+
+pub struct Renderer {
+    pub(crate) surface: Surface,
+    pub(crate) device: Device,
+    pub(crate) queue: Queue,
+    pub(crate) surface_configuration: SurfaceConfiguration,
+    pub(crate) size: PhysicalSize<u32>,
+
+    // Window must be dropped after the surface
+    pub(crate) window: Window
+}
+ 
+impl Renderer {
+    pub async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = Instance::new(InstanceDescriptor {
+            backends: Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = unsafe { instance.create_surface(&window).unwrap() };
+
+        let adapter = instance
+            .enumerate_adapters(wgpu::Backends::all())
+            .find(|adapter| {
+                // Check if this adapter supports our surface
+                adapter.is_surface_supported(&surface)
+            })
+            .unwrap();
+        
+        let (device, queue) = adapter.request_device(
+            &DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                label: None,
+            },
+            None,
+        ).await.unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps.formats.iter()
+            .copied()
+            .find(|f| f.is_srgb())            
+            .unwrap_or(surface_caps.formats[0]);
+        let config = SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+        
+        Self { 
+            surface,
+            device,
+            queue,
+            surface_configuration: config,
+            size,
+            window
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn input(&self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    pub fn update(&mut self) {
+        
+    }
+
+    pub fn render(&mut self) -> Result<(), SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+
+        let view = output.texture.create_view(&TextureViewDescriptor::default());
+        let mut encoder = self.device.create_command_encoder(&CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    pub fn resize(&mut self, x: u32, y: u32) {
+        if x > 0 {
+            self.size = PhysicalSize {
+                width: x,
+                height: self.size.height
+            };
+            self.surface_configuration.width = x;
+            self.surface.configure(&self.device, &self.surface_configuration);
+        }
+        if y > 0 {
+            self.size = PhysicalSize {
+                width: self.size.width,
+                height: y
+            };
+            self.surface_configuration.height = y;
+            self.surface.configure(&self.device, &self.surface_configuration);
+        }
+    }
+}
